6c676c804f694f0717ca7f105c1e5c9a6b1fb3f6
Paul Banjawan <paul.christian.banjawan@codeandtheory.com>
2021-04-06 12:53:13 +0800

    change region table colum to place to region

--- database/migrations/2021_03_31_014452_create_regions_table.php
+++ database/migrations/2021_03_31_014452_create_regions_table.php
@@ -15,7 +15,7 @@ class CreateRegionsTable extends Migration
     {
         Schema::create('regions', function (Blueprint $table) {
             $table->id();
-            $table->string('place');
+            $table->string('region');
             $table->timestamps();
         });
     }

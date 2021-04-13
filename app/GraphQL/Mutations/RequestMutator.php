1a5ff70513c05c14102584ebbfb4d7a121ceb95b
Paul Banjawan <paul.christian.banjawan@codeandtheory.com>
2021-04-13 10:10:01 +0800

    Added request mutator (create,update)

---
+++ app/GraphQL/Mutations/RequestMutator.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use App\GraphQL\Resolvers\BaseAuthResolver;
+use App\Models\Request;
+use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class RequestMutator extends BaseAuthResolver
+{
+    /**
+     * @param $rootValue
+     * @param array                                                    $args
+     * @param \Nuwave\Lighthouse\Support\Contracts\GraphQLContext|null $context
+     * @param \GraphQL\Type\Definition\ResolveInfo                     $resolveInfo
+     *
+     * @throws \Exception
+     *
+     * @return array
+     */
+    public function create($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    {
+        $args = collect($args);
+        $data = $args->toArray();
+        $request = Request::create($data);
+        return $request;
+    }
+
+    public function update($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
+    {
+      try{
+        $args = collect($args);
+        $request = Request::findOrFail($args->get('id'));
+        $request->update($args->toArray());
+      }catch (ModelNotFoundException $e) {
+        throw new ModelNotFoundException(__('Request not found.'), 400);
+      }
+        return $request;
+    }
+}

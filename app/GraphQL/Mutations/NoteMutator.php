b993bb78aaf43095f6a6c3e6e137d72e31f62ef2
Paul Banjawan <paul.christian.banjawan@codeandtheory.com>
2021-04-16 10:23:02 +0800

    Refactor Source and Note Mutators

--- app/GraphQL/Mutations/NoteMutator.php
+++ app/GraphQL/Mutations/NoteMutator.php
@@ -2,12 +2,14 @@
 
 namespace App\GraphQL\Mutations;
 
+use App\GraphQL\Resolvers\BaseAuthResolver;
 use App\Models\Note;
 use GraphQL\Type\Definition\ResolveInfo;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
-class NoteMutator
+class NoteMutator extends BaseAuthResolver
 {
     /**
      * @param $rootValue
@@ -23,10 +25,11 @@ class NoteMutator
     {
         $args = collect($args);
         $data = $args->except('directive')->toArray();
-
+        $user = Auth::user();
+        $data['user_id'] = $user->id;
         $note = Note::create($data);
 
-        return $note;
+        return $this->apiResponse('SUCCESS', 'Created a note.', $note);
     }
 
     /**
@@ -41,17 +44,24 @@ class NoteMutator
      */
     public function update($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
     {
-        try {
-            $args = collect($args);
-            $data = $args->except('directive')->toArray();
+        $args = collect($args);
+        $data = $args->except('directive')->toArray();
+        $user = Auth::user();
 
+        try {
             $note = Note::findOrFail($data['id']);
-            $note->update($data);
         } catch (ModelNotFoundException $e) {
-            throw new ModelNotFoundException(__('Note not found.'), 400);
+            return $this->apiResponse('INVALID_REQUEST', 'Note not found.');
+        }
+        $is_creator = $note->user()->where('id', '=', $user->id)->count();
+
+        if (!$is_creator) {
+            return $this->apiResponse('INVALID_REQUEST', 'You are not allowed to edit this note.');
         }
 
-        return $note;
+        $note->update($data);
+        
+        return $this->apiResponse('SUCCESS', 'Updated a note.', $note);
     }
 
     /**
